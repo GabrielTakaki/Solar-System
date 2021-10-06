136df4281dfec9fe0ee5d231afd090a76ca77c12
Gabriel Takaki <gabitj@icloud.com>
2021-10-06 17:27:29 -0300

    function to add planets

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 
+// Declarations
+var earthOrbitRadius = 100,
+    earthOrbitAngle = 0,
+    earthOrbitSpeed = 0.5,
+    
+    moonOrbitRadius = 10,
+    moonOrbitAngle = 0,
+    moonOrbitSpeed = 4;
+
 // THREE.JS COMMOM SETUP
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 3000 );
+camera.position.set( 0, 0, 0 );
 
 const renderer = new THREE.WebGL1Renderer();
 
@@ -19,33 +29,36 @@ let controls = new OrbitControls(camera, renderer.domElement);
 
 
 // Geometries
-let geometry = new THREE.SphereGeometry(1,100,50);
-geometry.scale(-2, 2, 2);
+const planets = [];
+let geometry, material;
+let timestamp = 0;
+let scaleVector = new THREE.Vector3();
 
-let material = new THREE.MeshBasicMaterial( {
-  map: new THREE.TextureLoader().load( './image/earthmap1k.jpg' ),
-  specularMap: new THREE.TextureLoader().load( './images/earthspec1k.jpg' ),
-  specular: new THREE.Color('gray')
-} );
-const cube = new THREE.Mesh ( geometry, material )
-scene.add( cube )
-scene.background = new THREE.Color(0x0000)
+const addPlanets = (sphX, posX, posY, posZ, texture, plan) => {
+  geometry = new THREE.SphereGeometry(sphX, 100, 50);
+  // geometry.scale(-2, 2, 2);
+  material = new THREE.MeshBasicMaterial( {
+    map: new THREE.TextureLoader().load( texture ),
+  } );
+  let mesh = new THREE.Mesh ( geometry, material )
+  scene.add( mesh )
+  scene.background = new THREE.Color(0x0000)
+  mesh.position.set(posX, posY, posZ)
+  planets.push(mesh)
+  console.log(planets)
+}
+
+camera.position.set(25,2,10)
 renderer.render( scene, camera )
-camera.position.z = 15
 
-geometry = new THREE.SphereGeometry( 0.370, 100, 100 )
-material = new THREE.MeshBasicMaterial( {
-  map: new THREE.TextureLoader().load( './image/2k_moon.jpg' ),
-} );
-const meshMoon = new THREE.Mesh ( geometry, material )
-meshMoon.position.set(10, 0, 0)
-scene.add( meshMoon )
+addPlanets(50, -200, 0, 0, './image/8k_sun.jpg')
+addPlanets(0.5, 20, 0, 0, './image/earthmap1k.jpg')
 
 geometry = new THREE.BufferGeometry();
 const vertices = [];
-for (let i = 0; i < 6000; i++) {
-  vertices.push(THREE.MathUtils.randFloatSpread(2000)); // x
-  vertices.push(THREE.MathUtils.randFloatSpread(2000)); // y
+for (let i = 0; i < 10000; i++) {
+  vertices.push(THREE.MathUtils.randFloatSpread(4000)); // x
+  vertices.push(THREE.MathUtils.randFloatSpread(4000)); // y
   vertices.push(THREE.MathUtils.randFloatSpread(2000)); // z
 }
 geometry.setAttribute(
@@ -82,10 +95,11 @@ function onWindowResize() {
 const animate = () => {
   requestAnimationFrame( animate );
   controls.update();
-  cube.rotation.y += 0.005;
-  cube.rotation.x += 0.001;
-  meshMoon.rotation.x += 0.0001
-  scene.rotation.y -= 0.003;
+  // let earthOrbitAngle += earthOrbitSpeed; 
+  // var radians = earthOrbitAngle * Math.PI / 180;
+
+  // planets[0].position.x = Math.cos(radians) * earthOrbitRadius;
+  // planets[0].position.z = Math.sin(radians) * earthOrbitRadius;
   renderer.render(scene,camera);
 }
 animate()
