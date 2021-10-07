0fa71b676fa05deed221c4944864ee3e86187187
Gabriel Takaki <gabitj@icloud.com>
2021-10-07 17:28:53 -0300

    better scene and camera position. Adds mars and jupter

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ import * as THREE from 'three';
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 
 // Declarations
-let earthOrbitRadius = 5000,
+// Distance from sun
+let earthOrbitRadius = 2500,
+    venusOrbitRadius = 1380,
+    mercuryOrbitRadius = 1000,
+    marsOrbitRadius = 3809,
+    jupterOrbitRadius = 13053,
     OrbitAngle = 0,
     OrbitSpeed = 0.5,
     
@@ -11,13 +16,11 @@ let earthOrbitRadius = 5000,
     moonOrbitAngle = 0,
     moonOrbitSpeed = 4;
 
-let mercuryOrbitRadius = 2000;
-let venusOrbitRadius = 2760;
 
 // THREE.JS COMMOM SETUP
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 60000 );
+const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 70000 );
 
 const renderer = new THREE.WebGL1Renderer();
 
@@ -50,15 +53,18 @@ const addPlanets = (sphX, posX, posY, posZ, texture) => {
   console.log(planets)
 }
 
-camera.position.set(0,400,2500)
+scene.position.set(0, 0, -1600)
+camera.position.set(-1400, 200, -500)
 renderer.render( scene, camera )
 
 addPlanets(595, 0, 0, 0, './image/8k_sun.jpg')
 addPlanets(5, 0, 0, 0, './image/earthmap1k.jpg')
 addPlanets(2.5, 0, 0, 0, './image/mercury.jpg')
 addPlanets(5.17, 0, 0, 0, './image/venus.jpg')
+addPlanets(10, 0, 0, 0, './image/mars.jpg')
+addPlanets(55, 0, 0, 0, './image/jupter.jpg')
 
-geometry = new THREE.SphereGeometry(15000,100,50);
+geometry = new THREE.SphereGeometry(20000,100,50);
 geometry.scale(-2, 2, 2);
 
 material = new THREE.MeshBasicMaterial( {
@@ -88,16 +94,23 @@ function onWindowResize() {
 
   renderer.setSize(innerWidth, innerHeight);
 }
+// planets[5].position.set(-500,0,0)
 
 function rotationAroundTheSun() {
+  // Days to orbit around the sun
   OrbitAngle += OrbitSpeed; 
   let earthRadians = OrbitAngle * Math.PI / 365;
   let mercuryRadians = OrbitAngle * Math.PI / 88;
   let venusRadians = OrbitAngle * Math.PI / 224;
+  let marsRadians = OrbitAngle * Math.PI / 687;
+  let jupterRadians = OrbitAngle * Math.PI / 4333;
 
   planets[0].rotation.x += 0.0002;
   planets[0].rotation.y += 0.0002;
   planets[1].rotation.x += 0.01;
+  planets[1].rotation.y += 0.01;
+  planets[5].rotation.x += 0.001
+  planets[5].rotation.y += 0.001
   
   // ROTATION AROUND THE SUN
   // Earth 
@@ -111,6 +124,14 @@ function rotationAroundTheSun() {
   // Venus
   planets[3].position.z = Math.sin(venusRadians) * venusOrbitRadius;
   planets[3].position.x = Math.cos(venusRadians) * venusOrbitRadius;
+
+  // Mars
+  planets[4].position.z = Math.sin(marsRadians) * marsOrbitRadius;
+  planets[4].position.x = Math.cos(marsRadians) * marsOrbitRadius;
+
+  // Jupter
+  planets[5].position.z = Math.sin(jupterRadians) * jupterOrbitRadius;
+  planets[5].position.x = Math.cos(jupterRadians) * jupterOrbitRadius;
 }
 
 const animate = () => {
